75dbc1fdb9ec1e769a6510de9534b625235ef6f0
Kamil Samigullin <kamil@samigullin.info>
2020-07-25 13:36:10 +0300

    describe features

--- go.mod
+++ go.mod
@@ -20,7 +20,7 @@ require (
 	github.com/stretchr/testify v1.6.1
 	go.octolab.org v0.2.0
 	go.octolab.org/toolkit/cli v0.1.1
-	go.octolab.org/toolkit/config v0.0.3
+	go.octolab.org/toolkit/config v0.0.4
 	golang.org/x/sync v0.0.0-20200625203802-6e8e738ad208
 )
 
--- internal/cnf/features.go
+++ internal/cnf/features.go
@@ -7,17 +7,31 @@ var Features = config.Features{
 	{
 		Name:    "cache",
 		Enabled: true,
+		Brief:   "Store state of metrics between calls.",
+		Docs:    "https://www.notion.so/octolab/Cache-layer-9766d3fbe07d4eb1808f6165fff8c5d0?r=0b753cbf767346f5a6fd51194829a2f3",
+	},
+	{
+		Name:    "debug",
+		Enabled: true,
+		Brief:   "Enable logger and run server with pprof.",
+		Docs:    "https://www.notion.so/octolab/Debugger-95877ccae2ee4168845c54066a443fe3?r=0b753cbf767346f5a6fd51194829a2f3",
 	},
 	{
 		Name:    "grafonnet",
 		Enabled: false,
+		Brief:   "Declare dashboard as code.",
+		Docs:    "https://www.notion.so/octolab/Grafonnet-3cd366ab76e146db82fd28f520bbdf68?r=0b753cbf767346f5a6fd51194829a2f3",
 	},
 	{
 		Name:    "paas",
 		Enabled: true,
+		Brief:   "Support app.toml and .env.paas to fetch configuration.",
+		Docs:    "https://www.notion.so/octolab/Avito-integration-066415cfcd914e20af7408e45272e98c?r=0b753cbf767346f5a6fd51194829a2f3",
 	},
 	{
 		Name:    "repl",
 		Enabled: true,
+		Brief:   "Enable interactive REPL mode to analyze data.",
+		Docs:    "https://www.notion.so/octolab/REPL-support-62f1301f41df4e8681527e268e10f539?r=0b753cbf767346f5a6fd51194829a2f3",
 	},
 }
